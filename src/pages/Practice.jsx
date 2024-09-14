b079504aabd6cd08e7aac3fd04c07c0e2190c2ce
Lance Hemphill <lance_hemphill1234@yahoo.com>
2024-09-14 19:55:04 -0400

    added streak counter with some styles

--- src/pages/Practice.jsx
+++ src/pages/Practice.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import { getProblemsByTypeAndDifficulty } from "../hooks/getProblems"
 import { getDifficulty } from "../context/setProblemParams"
 
+import practiceCSS from "../styles/Practice.module.css"
+
 const Practice = () => {
 
 
@@ -9,7 +11,9 @@ const Practice = () => {
     const [currentProblems, setCurrentProblems] = useState()
     const [randomGen, setRandomGen] = useState(0)
 
-    const currentDifficulty = getDifficulty()
+    const [streak, setStreak] = useState(0)
+
+
 
     useEffect(() => {
         
@@ -35,8 +39,11 @@ const Practice = () => {
         if (currentProblems[randomGen].answer === Number(guess)) {
             setRandomGen(Math.floor(Math.random() * currentProblems.length))
             setGuess("")
+            setStreak(streak + 1)
         }
-
+        else {
+            setStreak(0)
+        }
     }
 
     const handleGuess = (e) => {
@@ -48,15 +55,15 @@ const Practice = () => {
         <>
         <div>
 
-            <p>{currentDifficulty}</p>
-            <main>
+            <p className={practiceCSS.display_streak}>Streak: {streak}</p>
+            <main className={practiceCSS.main}>
+                
                 {currentProblems && <h1>{currentProblems[randomGen].problem} = ?</h1>}
-                <div>
-                    <input type="text" id="guess" name="guess" value={guess} onChange={e => setGuess(e.target.value)}/>
+                <div className={practiceCSS.input_container}>
+                    <input type="text" id="guess" name="guess" value={guess} onChange={e => setGuess(e.target.value)} className={practiceCSS.input_field}/>
                 </div>
 
-                <div>
-                    <button type="button" value={0} onClick={e => handleGuess(e.target.value)}>0</button>
+                <div className={practiceCSS.number_buttons}>
                     <button type="button" value={1} onClick={e => handleGuess(e.target.value)}>1</button>
                     <button type="button" value={2} onClick={e => handleGuess(e.target.value)}>2</button>
                     <button type="button" value={3} onClick={e => handleGuess(e.target.value)}>3</button>
@@ -66,9 +73,10 @@ const Practice = () => {
                     <button type="button" value={7} onClick={e => handleGuess(e.target.value)}>7</button>
                     <button type="button" value={8} onClick={e => handleGuess(e.target.value)}>8</button>
                     <button type="button" value={9} onClick={e => handleGuess(e.target.value)}>9</button>
-                    
+
                     <button type="button" onClick={() => setGuess(guess.slice(0, -1))}>DEL</button>
-                    <button type="button" onClick={check}>SUBMIT</button>
+                    <button type="button" value={0} onClick={e => handleGuess(e.target.value)}>0</button>
+                    <button type="button" onClick={check}>=</button>
                 </div>
 
                 
